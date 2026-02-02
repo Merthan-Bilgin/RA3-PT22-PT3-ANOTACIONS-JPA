44af7066e6127711d0e871ac4904531fbba6b4e7
Merthan-Bilgin <mbilginmehmed@gmail.com>
2026-02-02 17:20:53 +0100

    Clases plane y motorcicle

--- src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Motorcycle.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Motorcycle.java
@@ -1,5 +1,22 @@
 package es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain;
 
-public class Motorcycle extends Vehicle{
+import javax.persistence.*;
 
+// Moto, tipo concreto de Vehicle
+@Entity
+@Table(name = "motorcycle")
+@PrimaryKeyJoinColumn(name = "id") // Comparte id con vehicle
+public class Motorcycle extends Vehicle {
+
+	// Indica si la moto tiene sidecar
+	@Column(name = "hasSidecar")
+	private boolean hasSidecar;
+
+	public boolean isHasSidecar() {
+		return hasSidecar;
+	}
+
+	public void setHasSidecar(boolean hasSidecar) {
+		this.hasSidecar = hasSidecar;
+	}
 }
