af7e7bed7a18a71cf4bab42988677c28f0df4c92
Merthan-Bilgin <mbilginmehmed@gmail.com>
2026-02-02 17:06:18 +0100

    Clase teacher

--- src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Teacher.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Teacher.java
@@ -1,5 +1,19 @@
 package es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain;
 
-public class Teacher extends Person{
+import javax.persistence.*;
 
+@Entity
+@DiscriminatorValue("TEACHER") // Valor que se guardará en la columna person_type
+
+public class Teacher extends Person {
+	@Column(name = "teacherCode")
+	private String teacherCode;
+
+	public String getTeacherCode() {
+		return teacherCode;
+	}
+
+	public void setTeacherCode(String teacherCode) {
+		this.teacherCode = teacherCode;
+	}
 }
