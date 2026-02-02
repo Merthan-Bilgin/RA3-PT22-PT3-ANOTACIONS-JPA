d23d44247a5a68c1cefd9b9852672b808792c9e2
Merthan-Bilgin <mbilginmehmed@gmail.com>
2026-02-02 17:19:05 +0100

    Calse car

--- src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Car.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/anotacions/jpa/domain/Car.java
@@ -1,5 +1,23 @@
 package es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain;
 
-public class Car extends Vehicle{
+import javax.persistence.*;
 
+//Coche, tipo concreto de Vehicle
+@Entity
+@Table(name = "car")
+@PrimaryKeyJoinColumn(name = "id") // Usa la misma id que vehicle
+
+public class Car extends Vehicle {
+
+	// Número de puertas del coche
+	@Column(name = "doors")
+	private Integer doors;
+
+	public Integer getDoors() {
+		return doors;
+	}
+
+	public void setDoors(Integer doors) {
+		this.doors = doors;
+	}
 }
